94ffd2539f01c5aa608fce80af669dfd82fdfd0f
Rafael D'Andrea <rafael.dandrea@stonybrook.edu>
2023-06-26 18:56:31 -0400

    Update simulation_script.R

--- code/simulation_script.R
+++ code/simulation_script.R
@@ -5,23 +5,26 @@ library(furrr) ## for parallel computing
 library(gtools) ## for sampling
 
 ## fixed parameters
-totalspecies = 50
 fixed_parameters = 
-   list(
-      p0 = .7,
-      beta0 = 10,
-      lambda_min = 1,
-      lambda_max = 10,
-      gamma_min = .8,
-      gamma_max = 8,
-      alpha0 = 1e-17,
-      chain_length = totalspecies,
-      mu0 = .01,
-      rho0 = 2,
-      hmin = log(1),
-      hmax = log(8),
-      num_replicates = 50
-   ) 
+  list(
+    num_resources = 50,
+    num_species = 50,
+    p0 = .7,
+    beta_max = 10,
+    lambda_min = 1,
+    lambda_max = 10,
+    c_min = .8,
+    c_max = 8,
+    alpha0 = 1e-17,
+    mu = .01,
+    rho0 = 2,
+    tau = 1,
+    logh_min = log(1),
+    logh_max = log(8),
+    qmin = .05,
+    qmax = .1,
+    num_replicates = 50
+  ) 
 
 list2env(fixed_parameters, envir = .GlobalEnv)
 
@@ -29,8 +32,7 @@ list2env(fixed_parameters, envir = .GlobalEnv)
 source('project_functions.R')
 
 ## set number of workers for parallel processing
-# plan(multisession, workers = 120) 
-plan(cluster, workers = 200)
+plan(cluster, workers = 120)
 
 ## read batch array index (separates scenario simulations into chunks run by different nodes)
 array_id = 
@@ -42,17 +44,12 @@ array_id =
 scenarios = 
    expand_grid(
       n = c(seq(1, 17, 2), seq(19, 49, 5), 50),
-      sampling = 'random',
-      resource_supply = 'uniform',
-      ammonia = 'decrease',
-      density_dependence = c('none', 'neutral', 'intra', 'inter'),
-      production = c('serial', 'parallel', 'off'),
-      recalcitrance = TRUE,
-      handling_time = c('decrease', 'flat', 'increase'),
-      generalism = c('random', 'tradeoff', 'specialists'),
-      uptake_factor = 1,
-      efficiency = c('flat', 'increase'),
-      maxtime = 1000
+      ammonia_capacity = 'Decrease',
+      coregulation = c('Absent', 'Neutral', 'Intraspecific', 'Interspecific'),
+      byproduction = c('Serial', 'Nested', 'None'),
+      handling_time = c('Decrease', 'Flat', 'Increase'),
+      niches = c('Generalists', 'Gradient', 'Specialists'),
+      quality = c('Flat', 'Increase')
    ) |>
    rowid_to_column(var = 'scenario') |>
    expand_grid(seed = 1:num_replicates)
@@ -88,6 +85,7 @@ results =
    filter(scenario %in% minscen:maxscen) |>
    future_pmap_dfr(
       simulation,
+      t_max = 1000,
       full.data = TRUE,
       save.file = TRUE,
       path = dir_path,
