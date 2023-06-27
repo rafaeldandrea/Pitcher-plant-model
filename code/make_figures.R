3b1e9765e6f310ba746b9047bfcd03226b82c268
Rafael D'Andrea <rafael.dandrea@stonybrook.edu>
2023-06-27 10:59:02 -0500

    Update make_figures.R

--- code/make_figures.R
+++ code/make_figures.R
@@ -32,7 +32,8 @@ results_gradient =
 
 results_fig5 =
   "https://raw.githubusercontent.com/rafaeldandrea/Pitcher-plant-model/main/data/BEF_shape_table.csv" |>
-  read.csv()
+  read.csv() |>
+as_tibble()
 
 results_figS1 = 
   'https://github.com/rafaeldandrea/Pitcher-plant-model/raw/main/data/2023-06-16_results_figS1.rds' |>
